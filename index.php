75e87f4426ba01f40af7f83d6ebc69291f688deb
brennacr <58650762+brennacr@users.noreply.github.com>
2020-01-31 19:15:57 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Assingment 7</title>
+<meta charset="uft-8">
+</head>
+<body>
+<!--Brenna Rosenberg
+	Assignment7.php
+	INFO2340.WW
+	1/31/2020-->
+	<?php
+	$item = 'cereal';
+	$number = '2';
+	$place = 'gym';
+	$animal = 'penguin';
+?>
+	<h1>Good morning, Nebraska!</h1>
+	<div>Last night I ate <?php echo $item; ?>.</div>
+	<div>While it was good, it wasn't perfect. I'd give it <?php echo $number; ?> stars out of 4.</div>
+	<div>Tomorrow I plan to go to the  <?php echo $place; ?>.</div>
+	<div>My favorite animal is the  <?php echo $animal; ?>.</div>
+	</body>
+	</html>
